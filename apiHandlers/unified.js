d7393b1d7c682298c364d92e16678254c4bb2cc1
martin <martin@rga.com>
2015-03-19 13:52:31 -0500

    hey hey

--- apiHandlers/unified.js
+++ apiHandlers/unified.js
@@ -28,22 +28,22 @@ unified.story = function(artist, cb){
                             data[i].media.push(newOne);  
                         }
                         else {
-                            try{
-                                for (var z = 0; z < data[i].media.length; z++) {
-                                    var currentMedia = data[i].media[z];
-                                    var newId = newOne.oa_media_id.substring(0,newOne.oa_media_id.length-4);
-                                    var currentId = data.oa_media_id.substring(0,data.oa_media_id.length-4);
-                                    var isSameImage = newId.indexOf(currentId) > -1;
-                                    var isBigger = newOne.width > currentMedia.width;
-                                    if (isSameImage && isBigger){
-                                        data[i].media.push(newOne);    
-                                    }
+                            // try{
+                            //     for (var z = 0; z < data[i].media.length; z++) {
+                            //         var currentMedia = data[i].media[z];
+                            //         var newId = newOne.oa_media_id.substring(0,newOne.oa_media_id.length-4);
+                            //         var currentId = data.oa_media_id.substring(0,data.oa_media_id.length-4);
+                            //         var isSameImage = newId.indexOf(currentId) > -1;
+                            //         var isBigger = newOne.width > currentMedia.width;
+                            //         if (isSameImage && isBigger){
+                            //             data[i].media.push(newOne);    
+                            //         }
                                     
-                                };
-                            }catch(e){
-                                //console.log(e);
-                                data[i].media.push(newOne);
-                            }
+                            //     };
+                            // }catch(e){
+                            //     //console.log(e);
+                            //     data[i].media.push(newOne);
+                            // }
 
                         }
                     }  
