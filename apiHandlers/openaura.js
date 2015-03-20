85db0334ea1919ad707473714b75af6cce655f56
BrianNewsom <brian.newsom@colorado.edu>
2015-03-20 14:16:04 -0600

    Added some error handling so site doesnt crash

--- apiHandlers/openaura.js
+++ apiHandlers/openaura.js
@@ -6,23 +6,37 @@ openaura.getArtistImage = function(artist, cb){
     // Use decibel to get all genres for a given artist
     rest.get('http://api.openaura.com/v1/search/artists', {
         query : {'q' : artist, 'limit' : 1, 'api_key': api_key}}).on('complete', function(data){
-        var oa_artist_id = data[0].oa_artist_id;
+        try {
+            var oa_artist_id = data[0].oa_artist_id;
         rest.get('http://api.openaura.com/v1/classic/artists/' + oa_artist_id, {
             query: {'id_type' : 'oa:artist_id', 'api_key' : api_key }}).on('complete', function(data){
             cb(data.profile_image.url);
         })
+        }
+        catch(e){
+            console.log(e);
+            cb(null);
+            return 1;
+        }
     })
 };
 
 openaura.getSocialFeed = function(artist, limit, offset, cb){
     rest.get('http://api.openaura.com/v1/search/artists', {
         query : {'q' : artist, 'limit' : 1, 'api_key': api_key}}).on('complete', function(data){
-        var oa_artist_id = data[0].oa_artist_id;
-        rest.get('http://api.openaura.com/v1/particles/artists/' + oa_artist_id, {
-            // TODO: Get a particles api_key not jacked from their website example
-            query: {'id_type' : 'oa:artist_id','limit' : limit, 'offset' : offset, 'api_key' : 'zlA809tV1FCxCb55n5ei0mSmbtHgvpJe' }}).on('complete', function(data){
-            cb(data);
-        })
+        if(!data[0]){
+            console.log('no artist found');
+            cb(null);
+            return 1;
+        } else {
+            var oa_artist_id = data[0].oa_artist_id;
+            rest.get('http://api.openaura.com/v1/particles/artists/' + oa_artist_id, {
+                // TODO: Get a particles api_key not jacked from their website example
+                query: {'id_type' : 'oa:artist_id','limit' : limit, 'offset' : offset, 'api_key' : 'zlA809tV1FCxCb55n5ei0mSmbtHgvpJe' }}).on('complete', function(data){
+                cb(data);
+            })
+        }
+
     })
 };
 
--- apiHandlers/unified.js
+++ apiHandlers/unified.js
@@ -9,33 +9,39 @@ var unified = {};
 unified.story = function(artist, cb){
     setlistfm.getArtistGigs(artist, function(data){
         openaura.getSocialFeed(artist,250,0,function(result){
-            var media = result.particles;
-            for (var i = 0; i < data.length; i++) {
-                var current = moment(data[i].eventDate);
+            try{
+                var media = result.particles;
+                for (var i = 0; i < data.length; i++) {
+                    var current = moment(data[i].eventDate);
 
-                
-                for (var j = media.length - 1; j >= 0; j--) {
-                                    
 
-                    var mediaDate = moment(media[j].date);
-                    var daysBetween = current.diff(mediaDate, 'days',true); 
-                    
-                    if (daysBetween >=0 && daysBetween <= 3){
-                        var newOne = media[j].media;
-                              
-                        
-                        //Check if is smaller, not add it
-                        // if (newOne.width > 600){
-                            data[i].media = [];
-                            data[i].media.push(newOne);  
-                        // }
-                        
-                    }  
+                    for (var j = media.length - 1; j >= 0; j--) {
+
+
+                        var mediaDate = moment(media[j].date);
+                        var daysBetween = current.diff(mediaDate, 'days',true);
+
+                        if (daysBetween >=0 && daysBetween <= 3){
+                            var newOne = media[j].media;
+
+
+                            //Check if is smaller, not add it
+                            // if (newOne.width > 600){
+                                data[i].media = [];
+                                data[i].media.push(newOne);
+                            // }
+
+                        }
+                    };
                 };
-            };
 
-            cb(data);
-            return 0;
+                cb(data);
+                return 0;
+            }catch (e){
+                console.log(e);
+                cb(null);
+                return 1;
+            }
         })
 
     })
