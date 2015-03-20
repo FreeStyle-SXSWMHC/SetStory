1cc9485ddbbaaf8ea1d3ae1ce482928c398c27e0
Oscar <otech47@gmail.com>
2015-03-20 11:02:21 -0500

    setmine api
    hook to popularity

--- apiHandlers/setmine.js
+++ apiHandlers/setmine.js
@@ -20,4 +20,16 @@ setmine.init = function(callback) {
 
 }
 
+setmine.popularity = function(artist, event, callback) {
+	console.log(artist)
+	console.log(event)
+	rest.get("http://setmine.com/api/v/5/search", {
+		query: {
+			search: artist + " " + event
+		}}).on('complete', function(response) {
+			callback(response)
+		})
+
+}
+
 module.exports = setmine;
